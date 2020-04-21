970a26aa2207435f8aec577c676d50c469529473
eneas-hsn <eneashsn@gmail.com>
2020-04-21 15:47:41 -0300

    Create exercicio1.js

---
+++ CoDev/exerciciosaula2/exercicio1.js
@@ -0,0 +1,8 @@
+let projeto='CoEducaBr';
+let criado=2018;
+let site ='coeducabrasil.com.br';
+function codev() {
+    console.log(`O projeto ${projeto} foi criado em ${criado} e você pode acessar o site ${site} para saber mais.`);
+}
+console.log(codev());
+
